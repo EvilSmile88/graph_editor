bd2b04da898788bb0e8a797e267d4f538ff5505f
SOFTEQ\apodymov <artyom.podymov@softeq.com>
2019-05-28 11:00:33 +0300

    update arango db

--- services/DataServices.js
+++ services/DataServices.js
@@ -1,8 +1,18 @@
 const Database = require('arangojs');
 const aqlQuery = Database.aqlQuery;
-const db = new Database({ url: 'http://127.0.0.1:8529' });
+const db = new Database();
+
 db.useBasicAuth('root', '');
-db.useDatabase('graphDataBase');
+
+db.useDatabase('_system');
+const collection = db.collection('GraphData');
+
+if(!collection.exist) {
+    db.collection('GraphData').create().then(
+        () => console.log('Collection created'),
+        err => console.error('Failed to create collection:')
+    );
+}
 
 module.exports = {
 
@@ -10,33 +20,11 @@ module.exports = {
         try {
             const bindVars = {
                 user: 'artyom88',
-                nodes: [22242],
+                nodes: [222],
                 lines: [],
             };
-            // const cursor = await db.query('FOR x IN Graph FILTER x.user == @user UPDATE x WITH { user:@user, nodes:@nodes, lines:@lines } IN graph',bindVars );
-            const cursor = await db.query(aqlQuery`FOR x IN GraphData FILTER x.user == ${bindVars.user} UPDATE x WITH ${bindVars} IN GraphData`);
+            const cursor = await db.query(aqlQuery`UPSERT { user: ${bindVars.user}} INSERT ${bindVars} UPDATE ${bindVars} IN GraphData OPTIONS { ignoreRevs: false }`);
             const result = await cursor.all();
-            return result;
-        } catch (err) { console.log(err) }
-    },
-
-    getAllUsers : async function() {
-        try {
-            const cursor = await db.query('FOR x IN User RETURN x');
-            const result = await cursor.all();
-
-            return result;
-        } catch (err) { console.log(err) }
-    },
-
-    postUser : async function() {
-        const user = {
-            username: "xxxashish",
-            email: "xxxxashis@ashis.com"
-        };
-        try {
-            const cursor = await db.query(`INSERT ${user} IN User`);
-            const result = await cursor.next();
             return result;
         } catch (err) { console.log(err) }
     },

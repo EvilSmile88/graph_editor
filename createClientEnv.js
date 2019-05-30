8c3cc8384321c55ae751e5b112abbcec249de102
SOFTEQ\apodymov <artyom.podymov@softeq.com>
2019-05-30 11:02:29 +0300

    small fix

--- createClientEnv.js
+++ createClientEnv.js
@@ -1,9 +1,11 @@
 const path = require('path');
 const fs = require('fs');
+const PORT = process.env.PORT || 5000;
+
 const directoryPath = path.join(__dirname, 'client', 'src', 'js', 'env.js');
 
 const DEV_VARIABLES = {
-    API_URL: 'http://localhost:5000/api/'
+    API_URL: `http://localhost:${PORT}/api/`
 };
 
 const PROD_VARIABLES = {
